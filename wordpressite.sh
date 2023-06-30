2b5142eb8bbd3fb1b53376ee876dde1052859f29
GT1412 <81111020+GT1412@users.noreply.github.com>
2023-06-30 15:29:00 +0530

    Create wordpressite.sh

---
+++ wordpressite.sh
@@ -0,0 +1,12 @@
+#To create wordpress site
+./wordpress_management.sh create example.com
+
+#To enable the site
+./wordpress_management.sh enable example.com
+
+# To disable site
+./wordpress_management.sh disable example.com
+
+#to delete site
+./wordpress_management.sh delete example.com
+
